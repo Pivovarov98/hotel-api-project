7ef3384f3c34d83ce711b8c832b4eb22fd9b03be
Pivovarov98 <legion_19981@mail.ru>
2026-05-27 21:52:38 +0300

    feat: add access exception for unsuccessful login

---
+++ src/main/java/org/example/hotelapiproject/exeption/AccessDeniedException.java
@@ -0,0 +1,11 @@
+package org.example.hotelapiproject.exeption;
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String msg) {
+        super(msg);
+    }
+
+    public AccessDeniedException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
