bf63df7df7e1c4c1805a4e6e02286a01bfb232cf
Pivovarov98 <legion_19981@mail.ru>
2026-05-27 15:09:16 +0300

    Initial commit

---
+++ src/main/java/org/example/hotelapiproject/HotelApiProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.hotelapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelApiProjectApplication.class, args);
+    }
+
+}
