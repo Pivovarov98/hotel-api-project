3603ec6e24b75be68159484568a5f0aed3487a0d
Pivovarov98 <legion_19981@mail.ru>
2026-05-30 19:31:27 +0300

    feat: add account controller

---
+++ src/main/java/org/example/hotelapiproject/controller/AccountController.java
@@ -0,0 +1,47 @@
+package org.example.hotelapiproject.controller;
+
+import org.example.hotelapiproject.dto.account_dto.AccountChangePasswordDTO;
+import org.example.hotelapiproject.dto.account_dto.AccountCreateDTO;
+import org.example.hotelapiproject.dto.account_dto.AccountUpdateDTO;
+import org.example.hotelapiproject.entity.Account;
+import org.example.hotelapiproject.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/registration")
+    public ResponseEntity<Account> registration(@RequestBody AccountCreateDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(accountService.registration(dto));
+    }
+
+    @GetMapping("/{account_id}")
+    public Account findAccountByID(@PathVariable Long account_id) {
+        return accountService.findAccountByID(account_id);
+    }
+
+    @PatchMapping("/{account_id}")
+    public ResponseEntity<Account> updateAccount(@PathVariable Long account_id,
+                                                 @RequestBody AccountUpdateDTO dto) {
+        return ResponseEntity.ok(accountService.updateAccountByID(account_id, dto));
+    }
+
+    @PatchMapping("/change-password/{account_id}")
+    public ResponseEntity<Account> changePassword(@PathVariable Long account_id,
+                                                  @RequestBody AccountChangePasswordDTO dto) {
+        return ResponseEntity.ok(accountService.changePassword(account_id, dto));
+    }
+
+    @DeleteMapping("/{account_id}")
+    public ResponseEntity<Account> deleteAccountByID(@PathVariable Long account_id) {
+        accountService.deleteAccountByID(account_id);
+        return ResponseEntity.noContent().build();
+    }
+}
