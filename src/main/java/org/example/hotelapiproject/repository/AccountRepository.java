3d47847de3651bc38272ecda58c8237809077a1e
Pivovarov98 <legion_19981@mail.ru>
2026-06-01 23:05:09 +0300

    feat: add find by email method

--- src/main/java/org/example/hotelapiproject/repository/AccountRepository.java
+++ src/main/java/org/example/hotelapiproject/repository/AccountRepository.java
@@ -3,5 +3,9 @@ package org.example.hotelapiproject.repository;
 import org.example.hotelapiproject.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByEmail(String email);
+
 }
