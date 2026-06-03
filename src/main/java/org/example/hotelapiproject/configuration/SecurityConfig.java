dff67ff9fe6055b97c340e9f08dd8825bf61913e
Pivovarov98 <legion_19981@mail.ru>
2026-06-04 02:27:57 +0300

    feat: upadate security config

--- src/main/java/org/example/hotelapiproject/configuration/SecurityConfig.java
+++ src/main/java/org/example/hotelapiproject/configuration/SecurityConfig.java
@@ -19,22 +19,6 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .anyRequest().permitAll()
-//                );
-//
-//        return http.build();
-//    }
-//}
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -63,33 +47,6 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-//
-//                .httpBasic(AbstractHttpConfigurer::disable)
-//                .formLogin(AbstractHttpConfigurer::disable)
-//                .logout(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(authorize ->
-//                        authorize
-//                                .requestMatchers("/h2-console/**").permitAll()
-//                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-//                                .requestMatchers(HttpMethod.POST,  "/accounts/registration").permitAll()
-//                                .requestMatchers(HttpMethod.POST,  "/accounts/log_in").permitAll()
-//                                .requestMatchers(HttpMethod.POST,  "/accounts/refresh").permitAll()
-//                                .requestMatchers("/hotels/**").permitAll()
-//                                .requestMatchers("/rooms/**").permitAll()
-//                                .anyRequest().authenticated())
-//                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .exceptionHandling(c -> c.authenticationEntryPoint(authenticationEntryPoint()))
-//                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//        return http.build();
-//    }
 
     @Bean
     public AuthenticationEntryPoint authenticationEntryPoint() {
