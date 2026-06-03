8d55c182444cd5d4b68c8974960254caf79dfe0a
Pivovarov98 <legion_19981@mail.ru>
2026-06-04 02:30:49 +0300

    feat: update account and hotel response dto

--- src/main/java/org/example/hotelapiproject/dto/account_dto/AccountResponseDTO.java
+++ src/main/java/org/example/hotelapiproject/dto/account_dto/AccountResponseDTO.java
@@ -1,6 +1,7 @@
 package org.example.hotelapiproject.dto.account_dto;
 
 import lombok.Data;
+import org.example.hotelapiproject.dto.hotel_dto.HotelShortDTO;
 import org.example.hotelapiproject.entity.Hotel;
 
 import java.util.List;
@@ -11,5 +12,5 @@ public class AccountResponseDTO {
     private String email;
     private String name;
     private String surname;
-    private List<Hotel> hotels;
+    private List<HotelShortDTO> hotels;
 }
--- src/main/java/org/example/hotelapiproject/dto/hotel_dto/HotelResponseDTO.java
+++ src/main/java/org/example/hotelapiproject/dto/hotel_dto/HotelResponseDTO.java
@@ -1,8 +1,10 @@
 package org.example.hotelapiproject.dto.hotel_dto;
 
 import lombok.Data;
+import org.example.hotelapiproject.dto.room_dto.RoomShortDTO;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 public class HotelResponseDTO {
@@ -11,4 +13,5 @@ public class HotelResponseDTO {
     private String description;
     private BigDecimal latitude;
     private BigDecimal longitude;
+    private List<RoomShortDTO> rooms;
 }
