2a26d2ef0be0d5eb31e4da8b71066e290fb49637
Pivovarov98 <legion_19981@mail.ru>
2026-06-04 02:29:20 +0300

    feat: add booking controller

---
+++ src/main/java/org/example/hotelapiproject/controller/BookingController.java
@@ -0,0 +1,29 @@
+package org.example.hotelapiproject.controller;
+
+import org.example.hotelapiproject.dto.booking_dto.BookingCreateDTO;
+import org.example.hotelapiproject.dto.booking_dto.BookingResponseDTO;
+import org.example.hotelapiproject.entity.Booking;
+import org.example.hotelapiproject.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hotels/*/rooms/{room_id}/booking")
+public class BookingController {
+
+    @Autowired
+    BookingService bookingService;
+
+    @PostMapping()
+    public ResponseEntity<BookingResponseDTO> createBooking(@PathVariable Long room_id, @RequestBody BookingCreateDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookingService.createBooking(room_id, dto));
+    }
+
+    @DeleteMapping("/book_id")
+    public ResponseEntity<Booking> deleteBookById(@PathVariable Long book_id){
+        bookingService.deleteBookingByID(book_id);
+        return ResponseEntity.noContent().build();
+    }
+}
