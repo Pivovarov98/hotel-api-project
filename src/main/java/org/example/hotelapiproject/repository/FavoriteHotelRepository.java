ce4a9e1e332dc3c395d56de2e21c0d286b8cfca4
Pivovarov98 <legion_19981@mail.ru>
2026-06-06 17:37:25 +0300

    feat: update hotel and room repo, add methods

--- src/main/java/org/example/hotelapiproject/repository/FavoriteHotelRepository.java
+++ src/main/java/org/example/hotelapiproject/repository/FavoriteHotelRepository.java
@@ -6,18 +6,13 @@ import org.example.hotelapiproject.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FavoriteHotelRepository extends JpaRepository<FavoriteHotel, Long> {
 
-    boolean existsByAccountAndHotel(
-            Account account,
-            Hotel hotel
-    );
+    Optional<FavoriteHotel> findByAccountAndHotel(Account account, Hotel hotel);
 
-    void deleteByAccountAndHotel(
-            Account account,
-            Hotel hotel
-    );
+    boolean existsByAccountAndHotel(Account account, Hotel hotel);
 
     List<FavoriteHotel> findAllByAccount(Account account);
 }
--- src/main/java/org/example/hotelapiproject/repository/FavoriteRoomRepository.java
+++ src/main/java/org/example/hotelapiproject/repository/FavoriteRoomRepository.java
@@ -4,18 +4,13 @@ import org.example.hotelapiproject.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FavoriteRoomRepository extends JpaRepository<FavoriteRoom, Long> {
 
-    boolean existsByAccountAndRoom(
-            Account account,
-            Room room
-    );
+    Optional<FavoriteRoom> findByAccountAndRoom(Account account, Room room);
 
-    void deleteByAccountAndRoom(
-            Account account,
-            Room room
-    );
+    boolean existsByAccountAndRoom(Account account, Room room);
 
     List<FavoriteRoom> findAllByAccount(Account account);
 }
