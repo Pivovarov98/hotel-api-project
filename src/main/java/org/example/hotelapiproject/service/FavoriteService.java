a61b4377051812fc05e9c83637d0a2431d3ec531
Pivovarov98 <legion_19981@mail.ru>
2026-06-06 17:39:48 +0300

    feat: update favorite service, add toggle methods for add and delete favotite rooms or hotels, also add methods for get all favorites

--- src/main/java/org/example/hotelapiproject/service/FavoriteService.java
+++ src/main/java/org/example/hotelapiproject/service/FavoriteService.java
@@ -1,11 +1,16 @@
 package org.example.hotelapiproject.service;
 
-import org.example.hotelapiproject.entity.Account;
-import org.example.hotelapiproject.entity.FavoriteHotel;
-import org.example.hotelapiproject.repository.FavoriteHotelRepository;
-import org.example.hotelapiproject.repository.HotelRepository;
+import jakarta.transaction.Transactional;
+import org.example.hotelapiproject.dto.card.CardHotelResponseDTO;
+import org.example.hotelapiproject.dto.card.CardRoomResponseDTO;
+import org.example.hotelapiproject.entity.*;
+import org.example.hotelapiproject.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FavoriteService {
@@ -14,14 +19,105 @@ public class FavoriteService {
     HotelRepository hotelRepository;
 
     @Autowired
+    RoomRepository roomRepository;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
     FavoriteHotelRepository favoriteHotelRepository;
 
-    public FavoriteHotel addFavoriteHotel(Long hotel_id, Account account) {
-        FavoriteHotel newFavorite = new FavoriteHotel();
+    @Autowired
+    FavoriteRoomRepository favoriteRoomRepository;
 
-        newFavorite.setHotel(hotelRepository.getReferenceById(hotel_id));
+    @Transactional
+    public boolean favoriteHotelToggle(Long hotel_id) {
+        Account account = accountRepository.findByEmail(
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication()
+                        .getName()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        Hotel hotel = hotelRepository.findById(hotel_id)
+                .orElseThrow(() -> new RuntimeException("Hotel not found"));
+
+        Optional<FavoriteHotel> favorite = favoriteHotelRepository.findByAccountAndHotel(account, hotel);
+
+        if (favorite.isPresent()) {
+            favoriteHotelRepository.delete(favorite.get());
+            return false;
+        }
+
+        FavoriteHotel newFavorite = new FavoriteHotel();
+        newFavorite.setHotel(hotel);
         newFavorite.setAccount(account);
 
-        return favoriteHotelRepository.save(newFavorite);
+        favoriteHotelRepository.save(newFavorite);
+
+        return true;
+    }
+
+    public List<CardHotelResponseDTO> getAllFavoriteHotels() {
+        Account account = accountRepository.findByEmail(
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication()
+                        .getName()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        return favoriteHotelRepository.findAllByAccount(account)
+                .stream()
+                .map(favoriteHotel -> new CardHotelResponseDTO(
+                        favoriteHotel.getHotel().getId(),
+                        favoriteHotel.getHotel().getName(),
+                        favoriteHotel.getHotel().getDescription(),
+                        true
+                ))
+                .toList();
+    }
+
+    @Transactional
+    public boolean favoriteRoomToggle(Long room_id) {
+        Account account = accountRepository.findByEmail(
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication()
+                        .getName()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        Room room = roomRepository.findById(room_id)
+                .orElseThrow(() -> new RuntimeException("Room not found"));
+
+        Optional<FavoriteRoom> favorite = favoriteRoomRepository.findByAccountAndRoom(account, room);
+
+        if (favorite.isPresent()) {
+            favoriteRoomRepository.delete(favorite.get());
+            return false;
+        }
+
+        FavoriteRoom newFavorite = new FavoriteRoom();
+        newFavorite.setRoom(room);
+        newFavorite.setAccount(account);
+
+        favoriteRoomRepository.save(newFavorite);
+
+        return true;
+    }
+
+    public List<CardRoomResponseDTO> getAllFavoriteRooms() {
+        Account account = accountRepository.findByEmail(
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication()
+                        .getName()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        return favoriteRoomRepository.findAllByAccount(account)
+                .stream()
+                .map(favoriteRoom -> new CardRoomResponseDTO(
+                        favoriteRoom.getRoom().getId(),
+                        favoriteRoom.getRoom().getRoomTitle(),
+                        favoriteRoom.getRoom().getRoomDescription(),
+                        favoriteRoom.getRoom().getPrice(),
+                        true
+                ))
+                .toList();
     }
 }
