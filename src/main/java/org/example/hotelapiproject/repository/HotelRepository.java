9e7be19590709689bbb371db97f8cf00b7ab2a3c
Pivovarov98 <legion_19981@mail.ru>
2026-06-08 23:12:00 +0300

    feat: uodate hotel repository, add method to find hotels with min/mas price of its rooms

--- src/main/java/org/example/hotelapiproject/repository/HotelRepository.java
+++ src/main/java/org/example/hotelapiproject/repository/HotelRepository.java
@@ -2,6 +2,21 @@ package org.example.hotelapiproject.repository;
 
 import org.example.hotelapiproject.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
+
+    @Query("""
+            select distinct h
+            from Hotel h
+            join h.rooms r
+            where r.price between :minPrice and :maxPrice
+            """)
+    List<Hotel> findHotelByRoomPrise(
+            BigDecimal minPrice,
+            BigDecimal maxPrice
+    );
 }
