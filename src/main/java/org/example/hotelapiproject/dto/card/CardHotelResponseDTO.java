b094afee431d47c7fb768e2d5f3c82e7167989e4
Pivovarov98 <legion_19981@mail.ru>
2026-06-08 23:13:52 +0300

    feat: update card hotel response dto, add min price of available room

--- src/main/java/org/example/hotelapiproject/dto/card/CardHotelResponseDTO.java
+++ src/main/java/org/example/hotelapiproject/dto/card/CardHotelResponseDTO.java
@@ -1,8 +1,11 @@
 package org.example.hotelapiproject.dto.card;
 
+import java.math.BigDecimal;
+
 public record CardHotelResponseDTO(
         Long id,
         String name,
         String description,
+        BigDecimal minAvailablePrice,
         boolean favorite
 ) {}
