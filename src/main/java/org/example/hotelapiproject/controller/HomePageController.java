98ae7baffb137abf7b483acf5480cd7f989bee32
Pivovarov98 <legion_19981@mail.ru>
2026-06-19 23:50:14 +0300

    feat: update home page controller

--- src/main/java/org/example/hotelapiproject/controller/HomePageController.java
+++ src/main/java/org/example/hotelapiproject/controller/HomePageController.java
@@ -1,9 +1,14 @@
 package org.example.hotelapiproject.controller;
 
-import org.example.hotelapiproject.entity.HomePageService;
+import org.example.hotelapiproject.dto.card.CardHotelResponseDTO;
+import org.example.hotelapiproject.service.HomePageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -11,4 +16,10 @@ public class HomePageController {
 
     @Autowired
     HomePageService homePageService;
+
+
+    @GetMapping
+    ResponseEntity<List<CardHotelResponseDTO>> getAllHotels(){
+        return ResponseEntity.ok(homePageService.findAllHotels());
+    }
 }
