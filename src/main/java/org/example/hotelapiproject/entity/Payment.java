9d1e355f589abf3c8fc9164615cc72a00f7ac5b8
Pivovarov98 <legion_19981@mail.ru>
2026-06-25 22:40:15 +0300

    feat: add payment entity

---
+++ src/main/java/org/example/hotelapiproject/entity/Payment.java
@@ -0,0 +1,31 @@
+package org.example.hotelapiproject.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.hotelapiproject.entity.enums.PaymentStatus;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    Booking booking;
+
+    private BigDecimal amount;
+
+    private String providerPaymentId;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+}
