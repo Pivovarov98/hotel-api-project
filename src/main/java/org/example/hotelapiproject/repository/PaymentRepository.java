5cf91dadd91716b2f9e13d8cbac500c67f42fe78
Pivovarov98 <legion_19981@mail.ru>
2026-06-30 16:38:01 +0300

    feat: add search stripe session method

--- src/main/java/org/example/hotelapiproject/repository/PaymentRepository.java
+++ src/main/java/org/example/hotelapiproject/repository/PaymentRepository.java
@@ -3,5 +3,9 @@ package org.example.hotelapiproject.repository;
 import org.example.hotelapiproject.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByStripeSessionId(String stripeSessionId);
 }
