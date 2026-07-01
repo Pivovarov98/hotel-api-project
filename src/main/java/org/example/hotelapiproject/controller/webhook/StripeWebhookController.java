8a51eeef1b35662db1a921d7a23a8aa2de2f63bf
Pivovarov98 <legion_19981@mail.ru>
2026-07-01 16:28:32 +0300

    feat: rework stripe webhook

--- src/main/java/org/example/hotelapiproject/controller/webhook/StripeWebhookController.java
+++ src/main/java/org/example/hotelapiproject/controller/webhook/StripeWebhookController.java
@@ -1,19 +1,38 @@
 package org.example.hotelapiproject.controller.webhook;
 
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.model.Event;
+import com.stripe.model.checkout.Session;
+import com.stripe.net.Webhook;
+import org.example.hotelapiproject.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/webhook")
 public class StripeWebhookController {
 
+    @Value("${stripe.webhook.secret}")
+    private String endpointSecret;
+
+    @Autowired
+    private PaymentService paymentService;
+
     @PostMapping("/stripe")
-    public ResponseEntity<Void> handleWebhook(@RequestBody String payload) {
-        System.out.println("PAYMENT SUCCESS");
-        System.out.println(payload);
+    public ResponseEntity<Void> handleWebhook(@RequestBody String payload,
+                                              @RequestHeader("Stripe-Signature") String signature) throws SignatureVerificationException {
+
+        Event event = Webhook.constructEvent(payload, signature, endpointSecret);
+
+        if ("checkout.session.completed".equals(event.getType())){
+
+            Session session = (Session) event.getData().getObject();
+
+            paymentService.confirmPayment(session.getId());
+        }
         return ResponseEntity.ok().build();
     }
 }
