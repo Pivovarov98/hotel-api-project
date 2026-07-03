228e580ab094f98569de81ac018676286a690cf8
Pivovarov98 <legion_19981@mail.ru>
2026-07-03 23:04:07 +0300

    feat: add reiview controller

---
+++ src/main/java/org/example/hotelapiproject/controller/ReviewController.java
@@ -0,0 +1,10 @@
+package org.example.hotelapiproject.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hotel/{hotel_id}/reviwe")
+public class ReviewController {
+}
