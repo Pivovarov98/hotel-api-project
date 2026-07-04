fb8eb76a6f9d19e99757277f886490f073689fe4
Pivovarov98 <legion_19981@mail.ru>
2026-07-04 23:42:45 +0300

    feat: add service

--- src/main/java/org/example/hotelapiproject/controller/ReviewController.java
+++ src/main/java/org/example/hotelapiproject/controller/ReviewController.java
@@ -1,10 +1,15 @@
 package org.example.hotelapiproject.controller;
 
 
+import org.example.hotelapiproject.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hotel/{hotel_id}/reviwe")
 public class ReviewController {
+
+    @Autowired
+    ReviewService reviewService;
 }
