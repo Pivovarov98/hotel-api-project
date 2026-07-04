15006358f2dc5ff4a5c568a6fa2b48fd52de7201
Pivovarov98 <legion_19981@mail.ru>
2026-07-04 23:43:40 +0300

    feat: add create method

--- src/main/java/org/example/hotelapiproject/service/ReviewService.java
+++ src/main/java/org/example/hotelapiproject/service/ReviewService.java
@@ -1,7 +1,26 @@
 package org.example.hotelapiproject.service;
 
+import org.example.hotelapiproject.entity.Review;
+import org.example.hotelapiproject.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    public ReviewResponceDTO createReview(CreateReviewDTO dto){
+
+        Review review = Review.builder()
+                .reviewTitle(dto.getTitle)
+                .reviewDescription(dto.getDescription)
+                .rating(dto.getRating)
+                .hotel(dto.getHotelId)
+                .build();
+
+        reviewRepository.save(review);
+        return responceDTO(review);
+    }
 }
