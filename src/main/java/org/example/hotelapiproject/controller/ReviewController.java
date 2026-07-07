a82e9eeadc2c32e289008059256adc495dcaaa61
Pivovarov98 <legion_19981@mail.ru>
2026-07-07 13:20:25 +0300

    feat: add endpoints for review

--- src/main/java/org/example/hotelapiproject/controller/ReviewController.java
+++ src/main/java/org/example/hotelapiproject/controller/ReviewController.java
@@ -1,15 +1,39 @@
 package org.example.hotelapiproject.controller;
 
 
+import org.example.hotelapiproject.dto.review_dto.CreateReviewDTO;
+import org.example.hotelapiproject.dto.review_dto.ReviewResponseDTO;
+import org.example.hotelapiproject.dto.review_dto.UpdateReviewDTO;
 import org.example.hotelapiproject.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/hotel/{hotel_id}/reviwe")
+@RequestMapping
 public class ReviewController {
 
     @Autowired
     ReviewService reviewService;
+
+    @PostMapping("/hotel/reviwe/new")
+    public ResponseEntity<ReviewResponseDTO> createReview(@RequestBody CreateReviewDTO dto){
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(reviewService.createReview(dto));
+    }
+
+    @PatchMapping("/reviwe/{review_id}")
+    public ResponseEntity<ReviewResponseDTO> updateReview(@PathVariable Long review_id,
+                                                          @RequestBody UpdateReviewDTO dto){
+
+        return ResponseEntity.ok().body(reviewService.updateReview(review_id, dto));
+    }
+
+    @DeleteMapping("/reviwe/{review_id}")
+    public ResponseEntity<Void> deleteReview(@PathVariable Long review_id){
+
+        reviewService.deleteReview(review_id);
+        return ResponseEntity.noContent().build();
+    }
 }
