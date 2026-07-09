27182354db4c2590728a42d4d99ff78287dfb64b
Pivovarov98 <legion_19981@mail.ru>
2026-07-09 23:06:28 +0300

    feat: add builder

--- src/main/java/org/example/hotelapiproject/dto/review_dto/UpdateReviewDTO.java
+++ src/main/java/org/example/hotelapiproject/dto/review_dto/UpdateReviewDTO.java
@@ -1,8 +1,10 @@
 package org.example.hotelapiproject.dto.review_dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UpdateReviewDTO {
     String title;
     String description;
