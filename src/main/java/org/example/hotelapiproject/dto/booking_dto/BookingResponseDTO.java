7d056647628036845b13d6f25a7d798f249116e8
Pivovarov98 <legion_19981@mail.ru>
2026-07-10 21:44:01 +0300

    feat: add builder

--- src/main/java/org/example/hotelapiproject/dto/booking_dto/BookingResponseDTO.java
+++ src/main/java/org/example/hotelapiproject/dto/booking_dto/BookingResponseDTO.java
@@ -1,10 +1,12 @@
 package org.example.hotelapiproject.dto.booking_dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class BookingResponseDTO {
 
     private Long id;
