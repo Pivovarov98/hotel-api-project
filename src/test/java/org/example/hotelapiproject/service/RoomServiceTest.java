066556d9451ecb0ab0530da9fc8bc4343ddd0476
Pivovarov98 <legion_19981@mail.ru>
2026-07-10 21:46:39 +0300

    feat: add test for find, update and delete room methods

--- src/test/java/org/example/hotelapiproject/service/RoomServiceTest.java
+++ src/test/java/org/example/hotelapiproject/service/RoomServiceTest.java
@@ -2,6 +2,7 @@ package org.example.hotelapiproject.service;
 
 import org.example.hotelapiproject.dto.room_dto.RoomCreateDTO;
 import org.example.hotelapiproject.dto.room_dto.RoomResponseDTO;
+import org.example.hotelapiproject.dto.room_dto.RoomUpdateDTO;
 import org.example.hotelapiproject.entity.Hotel;
 import org.example.hotelapiproject.entity.Room;
 import org.example.hotelapiproject.repository.HotelRepository;
@@ -13,7 +14,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -64,13 +64,80 @@ class RoomServiceTest {
 
     @Test
     void findRoomByID() {
+
+        Hotel hotel = new Hotel();
+        hotel.setId(1L);
+
+        Room room = Room.builder()
+                .id(28L)
+                .price(BigDecimal.valueOf(400))
+                .roomTitle("Test room title")
+                .roomDescription("Test room description")
+                .hotel(hotel)
+                .build();
+
+        when(roomRepository.getReferenceById(anyLong()))
+                .thenReturn(room);
+
+        RoomResponseDTO responseDTO = roomService.findRoomByID(room.getId());
+
+        assertEquals(BigDecimal.valueOf(400), responseDTO.getPrice());
+        assertEquals("Test room title", responseDTO.getRoomTitle());
+        assertEquals("Test room description", responseDTO.getRoomDescription());
     }
 
     @Test
     void updateRoomByID() {
+
+        Hotel hotel = new Hotel();
+        hotel.setId(1L);
+
+        Room room = Room.builder()
+                .id(30L)
+                .price(BigDecimal.valueOf(400))
+                .roomTitle("Test old room title")
+                .roomDescription("Test old room description")
+                .hotel(hotel)
+                .build();
+
+        RoomUpdateDTO dto = RoomUpdateDTO.builder()
+                .price(BigDecimal.valueOf(500))
+                .roomTitle("New room title")
+                .roomDescription("New room description")
+                .build();
+
+        when(roomRepository.getReferenceById(anyLong()))
+                .thenReturn(room);
+
+        when(roomRepository.save(any(Room.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        RoomResponseDTO responseDTO = roomService.updateRoomByID(room.getId(), dto);
+
+        assertEquals(BigDecimal.valueOf(500), responseDTO.getPrice());
+        assertEquals("New room title", responseDTO.getRoomTitle());
+        assertEquals("New room description", responseDTO.getRoomDescription());
     }
 
     @Test
     void deleteRoomByID() {
+
+        Hotel hotel = new Hotel();
+        hotel.setId(1L);
+
+        Room room = Room.builder()
+                .id(30L)
+                .price(BigDecimal.valueOf(400))
+                .roomTitle("Test old room title")
+                .roomDescription("Test old room description")
+                .hotel(hotel)
+                .build();
+
+        when(roomRepository.existsById(anyLong()))
+                .thenReturn(true);
+
+        roomService.deleteRoomByID(room.getId());
+
+        verify(roomRepository).deleteById(room.getId());
     }
 }
