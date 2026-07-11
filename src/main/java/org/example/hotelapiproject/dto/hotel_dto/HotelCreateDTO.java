cd7e06a18f7dae521dc331615145a745e6005260
Pivovarov98 <legion_19981@mail.ru>
2026-07-11 15:35:39 +0300

    feat: add builder

--- src/main/java/org/example/hotelapiproject/dto/hotel_dto/HotelCreateDTO.java
+++ src/main/java/org/example/hotelapiproject/dto/hotel_dto/HotelCreateDTO.java
@@ -2,11 +2,13 @@ package org.example.hotelapiproject.dto.hotel_dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 public class HotelCreateDTO {
 
     @NotBlank
--- src/main/java/org/example/hotelapiproject/dto/hotel_dto/HotelUpdateDTO.java
+++ src/main/java/org/example/hotelapiproject/dto/hotel_dto/HotelUpdateDTO.java
@@ -1,10 +1,12 @@
 package org.example.hotelapiproject.dto.hotel_dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 public class HotelUpdateDTO {
 
     private String name;
