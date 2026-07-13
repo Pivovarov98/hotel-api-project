5755224e6b4222366513328972743098a0a00ecb
Pivovarov98 <legion_19981@mail.ru>
2026-07-13 14:21:44 +0300

    feat: add payment success test

--- src/test/java/org/example/hotelapiproject/service/PaymentServiceTest.java
+++ src/test/java/org/example/hotelapiproject/service/PaymentServiceTest.java
@@ -1,5 +1,9 @@
 package org.example.hotelapiproject.service;
 
+import org.example.hotelapiproject.entity.Booking;
+import org.example.hotelapiproject.entity.Payment;
+import org.example.hotelapiproject.entity.enums.BookingStatus;
+import org.example.hotelapiproject.entity.enums.PaymentStatus;
 import org.example.hotelapiproject.exeption.payment.PaymentNotFoundException;
 import org.example.hotelapiproject.repository.BookingRepository;
 import org.example.hotelapiproject.repository.PaymentRepository;
@@ -9,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -31,7 +36,21 @@ class PaymentServiceTest {
     }
 
     @Test
-    void confirmPayment() {
+    void confirmPaymentPaymentSuccess() {
+
+        Payment payment = mockPayment();
+
+        when(paymentRepository.findByStripeSessionId("stripeSession"))
+                .thenReturn(Optional.of(payment));
+
+        when(paymentRepository.save(any(Payment.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        paymentService.confirmPayment("stripeSession");
+
+        assertEquals(PaymentStatus.SUCCESS, payment.getStatus());
+
+        verify(paymentRepository).save(payment);
     }
 
     @Test
@@ -45,6 +64,21 @@ class PaymentServiceTest {
                 () -> paymentService.confirmPayment("session123"));
 
         assertEquals("Payment not found", exception.getMessage());
+    }
+
+    private Payment mockPayment() {
+        return Payment.builder()
+                .id(7L)
+                .booking(Booking.builder()
+                        .id(3L)
+                        .status(BookingStatus.PENDING)
+                        .build())
+                .amount(BigDecimal.valueOf(235))
+                .providerPaymentId("123-qwer-456-asdf")
+                .status(PaymentStatus.PENDING)
+                .stripeSessionId("stripeSession")
+                .stripePaymentIntentId("stripePaymentIntent")
+                .build();
     }
 
 }
