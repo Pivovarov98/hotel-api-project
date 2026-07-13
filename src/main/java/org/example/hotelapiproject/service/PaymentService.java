23ca12263e85d3286988396c5c5f2a15ef9d934c
Pivovarov98 <legion_19981@mail.ru>
2026-07-13 14:34:49 +0300

    feat: rework payment service

--- src/main/java/org/example/hotelapiproject/service/PaymentService.java
+++ src/main/java/org/example/hotelapiproject/service/PaymentService.java
@@ -1,8 +1,6 @@
 package org.example.hotelapiproject.service;
 
 import com.stripe.exception.StripeException;
-import com.stripe.model.checkout.Session;
-import com.stripe.param.checkout.SessionCreateParams;
 import jakarta.transaction.Transactional;
 import org.example.hotelapiproject.dto.checkout_session_dto.CheckoutSessionData;
 import org.example.hotelapiproject.dto.payments_dto.PaymentResponse;
@@ -13,25 +11,27 @@ import org.example.hotelapiproject.entity.enums.PaymentStatus;
 import org.example.hotelapiproject.exeption.payment.PaymentNotFoundException;
 import org.example.hotelapiproject.repository.BookingRepository;
 import org.example.hotelapiproject.repository.PaymentRepository;
+import org.example.hotelapiproject.service.payment_integration.StripeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
 
 @Service
 public class PaymentService {
 
     @Autowired
-    PaymentRepository paymentRepository;
+    private PaymentRepository paymentRepository;
 
     @Autowired
-    BookingRepository bookingRepository;
+    private BookingRepository bookingRepository;
+
+    @Autowired
+    private StripeService stripeService;
 
     public PaymentResponse createPayment(Long booking_id) throws StripeException {
         Booking booking = bookingRepository.findById(booking_id)
                 .orElseThrow(() -> new RuntimeException("Book not find"));
 
-        CheckoutSessionData sessionData = createCheckoutSession(booking);
+        CheckoutSessionData sessionData = stripeService.createCheckoutSession(booking);
 
         Payment payment = Payment.builder()
                 .booking(booking)
@@ -62,44 +62,5 @@ public class PaymentService {
 
         bookingRepository.save(booking);
         paymentRepository.save(payment);
-    }
-
-    public CheckoutSessionData createCheckoutSession(Booking booking) throws StripeException {
-        SessionCreateParams params = SessionCreateParams.builder()
-                .setMode(SessionCreateParams.Mode.PAYMENT)
-                .setSuccessUrl("http://localhost:8080/api/v1/success")
-                .setCancelUrl("http://localhost:8080/api/v1/cancel")
-                .addLineItem(
-                        SessionCreateParams.LineItem.builder()
-                                .setQuantity(1L)
-                                .setPriceData(SessionCreateParams
-                                        .LineItem
-                                        .PriceData
-                                        .builder()
-                                        .setCurrency("eur")
-                                        .setUnitAmount(
-                                                booking
-                                                        .getTotalPrice()
-                                                        .multiply(BigDecimal.valueOf(100))
-                                                        .longValue()
-                                        )
-                                        .setProductData(
-                                                SessionCreateParams
-                                                        .LineItem
-                                                        .PriceData
-                                                        .ProductData
-                                                        .builder()
-                                                        .setName("Hotel booking")
-                                                        .build()
-                                        )
-                                        .build()
-                                )
-                                .build()
-                )
-                .build();
-
-        Session session = Session.create(params);
-
-        return new CheckoutSessionData(session.getId(), session.getUrl());
     }
 }
