07d82cce2e7ff2b154d40ba65b1e665920b03057
Pivovarov98 <legion_19981@mail.ru>
2026-07-19 23:50:16 +0300

    feat: add test for hotel controller delete hotel method

---
+++ src/test/java/org/example/hotelapiproject/controller/HotelControllerTest.java
@@ -0,0 +1,75 @@
+package org.example.hotelapiproject.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.hotelapiproject.dto.hotel_dto.HotelCreateDTO;
+import org.example.hotelapiproject.dto.hotel_dto.HotelResponseDTO;
+import org.example.hotelapiproject.service.HotelService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(HotelController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class HotelControllerTest {
+
+    @MockitoBean
+    private HotelService hotelService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void createHotel() throws Exception {
+
+        HotelCreateDTO createDTO = HotelCreateDTO.builder()
+                .name("Star Hotel")
+                .description("Test desc")
+                .latitude(BigDecimal.valueOf(158.74))
+                .longitude(BigDecimal.valueOf(845.98))
+                .build();
+
+        HotelResponseDTO response = HotelResponseDTO.builder()
+                .name("Star Hotel")
+                .description("Test desc")
+                .latitude(BigDecimal.valueOf(158.74))
+                .longitude(BigDecimal.valueOf(845.98))
+                .build();
+
+        when(hotelService.create(any()))
+                .thenReturn(response);
+
+        mockMvc.perform(post("/hotels/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value(response.getName()))
+                .andExpect(jsonPath("$.description").value(response.getDescription()))
+                .andExpect(jsonPath("$.latitude").value(response.getLatitude()))
+                .andExpect(jsonPath("$.longitude").value(response.getLongitude()));
+    }
+
+    @Test
+    void findHotelByID() {
+    }
+
+    @Test
+    void updateHotelInfo() {
+    }
+
+    @Test
+    void deleteHotelByID() {
+    }
+}
