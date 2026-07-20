3375f56eb424c085ecde68647acf4697cdbbcb9e
Pivovarov98 <legion_19981@mail.ru>
2026-07-20 14:01:03 +0300

    feat: add test for favorite controller add and remove favorite hotel method

---
+++ src/test/java/org/example/hotelapiproject/controller/FavoriteControllerTest.java
@@ -0,0 +1,66 @@
+package org.example.hotelapiproject.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.hotelapiproject.service.FavoriteService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(FavoriteController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class FavoriteControllerTest {
+
+    @MockitoBean
+    private FavoriteService favoriteService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void favoriteHotelToggleAddFavorite() throws Exception{
+
+        when(favoriteService.favoriteHotelToggle(anyLong()))
+                .thenReturn(true);
+
+        mockMvc.perform(post("/favorite/hotels/5"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.favorite").value(true));
+
+        verify(favoriteService).favoriteHotelToggle(5L);
+    }
+
+    @Test
+    void favoriteHotelToggleRemoveFavorite() throws Exception{
+
+        when(favoriteService.favoriteHotelToggle(anyLong()))
+                .thenReturn(false);
+
+        mockMvc.perform(post("/favorite/hotels/5"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.favorite").value(false));
+
+        verify(favoriteService).favoriteHotelToggle(5L);
+    }
+
+    @Test
+    void getFavoriteHotels() throws Exception{
+    }
+
+    @Test
+    void favoriteRoomToggle() throws Exception{
+    }
+
+    @Test
+    void getFavoriteRooms() throws Exception{
+    }
+}
