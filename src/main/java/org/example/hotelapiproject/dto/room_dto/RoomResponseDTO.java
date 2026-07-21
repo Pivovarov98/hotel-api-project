3311a7340f523cf5c2e64611eea91b5f2b34eaab
Pivovarov98 <legion_19981@mail.ru>
2026-07-21 12:07:00 +0300

    feat: add builder and constructors

--- src/main/java/org/example/hotelapiproject/dto/room_dto/RoomResponseDTO.java
+++ src/main/java/org/example/hotelapiproject/dto/room_dto/RoomResponseDTO.java
@@ -1,10 +1,16 @@
 package org.example.hotelapiproject.dto.room_dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RoomResponseDTO {
 
     private Long hotelId;
