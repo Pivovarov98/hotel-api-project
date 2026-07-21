6280bd59152dd4ce1c462e4afe79ffc89c42a23d
Pivovarov98 <legion_19981@mail.ru>
2026-07-21 12:50:59 +0300

    feat: add test for account controller create and delete methods

---
+++ src/test/java/org/example/hotelapiproject/controller/AccountControllerTest.java
@@ -0,0 +1,86 @@
+package org.example.hotelapiproject.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.hotelapiproject.dto.account_dto.AccountCreateDTO;
+import org.example.hotelapiproject.dto.account_dto.AccountResponseDTO;
+import org.example.hotelapiproject.service.AccountService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(AccountController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class AccountControllerTest {
+
+    @MockitoBean
+    private AccountService accountService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void registration() throws Exception {
+
+        AccountCreateDTO createDTO = AccountCreateDTO.builder()
+                .email("Test@test.com")
+                .name("Egor")
+                .surname("Piv")
+                .password("test123")
+                .repeatPassword("test123")
+                .isHotelOwner(false)
+                .build();
+
+        AccountResponseDTO response = AccountResponseDTO.builder()
+                .email(createDTO.getEmail())
+                .surname(createDTO.getSurname())
+                .name(createDTO.getName())
+                .build();
+
+        when(accountService.registration(any()))
+                .thenReturn(response);
+
+        mockMvc.perform(post("/accounts/registration")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value(response.getName()))
+                .andExpect(jsonPath("$.surname").value(response.getSurname()))
+                .andExpect(jsonPath("$.email").value(response.getEmail()));
+    }
+
+    @Test
+    void auth() throws Exception {
+    }
+
+    @Test
+    void findAccountByID() throws Exception {
+    }
+
+    @Test
+    void updateAccount() throws Exception {
+    }
+
+    @Test
+    void changePassword() throws Exception {
+    }
+
+    @Test
+    void deleteAccountByID() throws Exception {
+
+        mockMvc.perform(delete("/accounts/1"))
+                .andExpect(status().isNoContent());
+
+        verify(accountService).deleteAccountByID(1L);
+    }
+}
