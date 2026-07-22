7e3c13f21ea929e7b150e9f70960f44ab0dfbca5
Pivovarov98 <legion_19981@mail.ru>
2026-07-22 22:40:45 +0300

    feat: add get all method

--- src/main/java/org/example/hotelapiproject/service/HomePageService.java
+++ src/main/java/org/example/hotelapiproject/service/HomePageService.java
@@ -1,7 +1,19 @@
 package org.example.hotelapiproject.service;
 
+import org.example.hotelapiproject.dto.card.CardHotelResponseDTO;
+import org.example.hotelapiproject.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class HomePageService {
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    public List<CardHotelResponseDTO> getAllHotels() {
+
+    }
 }
